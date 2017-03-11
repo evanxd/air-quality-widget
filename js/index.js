f0c879cba6cb09f87da22942ab6016ddd721bcee
Evan Tseng <evan@tseng.io>
2017-03-11 15:32:39 +0800

    Get MQTT server info from URL params

--- js/index.js
+++ js/index.js
@@ -6,14 +6,19 @@ var MQTT_CLIENT_ID = "clientId-Qqfjd";
 var MQTT_TOPIC = "topic";
 
 (function() {
+  var params = new URLSearchParams(window.location.search);
+  var mqttServerAddress = params.get("mqttServerAddress") || MQTT_SERVER_ADDRESS;
+  var mqttServerPort = Number(params.get("mqttServerPort")) || MQTT_SERVER_PORT;
+  var mqttTopic = params.get("mqttTopic") || MQTT_TOPIC;
+
   var config = liquidFillGaugeDefaultSettings();
   config.displayPercent = false;
   var airQualityGauge = loadLiquidFillGauge("air-quality-widget", 0, config);
-  var client = new Paho.MQTT.Client(MQTT_SERVER_ADDRESS, MQTT_SERVER_PORT, MQTT_CLIENT_ID);
+  var client = new Paho.MQTT.Client(mqttServerAddress, mqttServerPort, MQTT_CLIENT_ID);
 
   client.connect({ onSuccess: function() {
     console.log("Connected with the MQTT server.");
-    client.subscribe(MQTT_TOPIC);
+    client.subscribe(mqttTopic);
   }});
 
   client.onMessageArrived = function onMessageArrived(message) {
