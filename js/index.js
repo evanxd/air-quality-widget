3372653976358ad7572ed3bcc5b23b549a241e31
Evan Tseng <evan@tseng.io>
2017-03-11 21:41:18 +0800

    Generate and assign the unique client ID

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@
 
 var MQTT_SERVER_ADDRESS = "broker.mqtt-dashboard.com";
 var MQTT_SERVER_PORT = 8000;
-var MQTT_CLIENT_ID = "clientId-Qqfjd";
 var MQTT_TOPIC = "topic";
 
 (function() {
@@ -10,11 +9,12 @@ var MQTT_TOPIC = "topic";
   var mqttServerAddress = params.get("server") || MQTT_SERVER_ADDRESS;
   var mqttServerPort = Number(params.get("port")) || MQTT_SERVER_PORT;
   var mqttTopic = params.get("topic") || MQTT_TOPIC;
+  var mqttClientId = uuid.v4();
 
   var config = liquidFillGaugeDefaultSettings();
   config.displayPercent = false;
   var airQualityGauge = loadLiquidFillGauge("air-quality-widget", 0, config);
-  var client = new Paho.MQTT.Client(mqttServerAddress, mqttServerPort, MQTT_CLIENT_ID);
+  var client = new Paho.MQTT.Client(mqttServerAddress, mqttServerPort, mqttClientId);
 
   client.connect({ onSuccess: function() {
     console.log("Connected with the MQTT server.");
