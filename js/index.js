605236d01a8cbef4f21768f6827457b6eea25eee
Evan Tseng <evan@tseng.io>
2017-03-12 13:33:25 +0800

    Show the one-minute average air quality data

--- js/index.js
+++ js/index.js
@@ -15,11 +15,20 @@ var MQTT_TOPIC = "topic";
   config.displayPercent = false;
   var airQualityGauge = loadLiquidFillGauge("air-quality-widget", 0, config);
 
+  // Show the real-time one-minute average data on the widget.
+  // Workaround: Base on that the data sampling rate is a piece of data per two seconds,
+  // we assume that the widget can receive 30 piece of data in a minute.
+  var averageData = 0;
+  var dataAmount = 0;
   var client = new Paho.MQTT.Client(mqttServerAddress, mqttServerPort, mqttClientId);
   client.onMessageArrived = function onMessageArrived(message) {
     console.log("data: " + message.payloadString);
+    if (dataAmount < 30) {
+      dataAmount++;
+    }
     var data = JSON.parse(message.payloadString);
-    airQualityGauge.update(data.pm2_5);
+    averageData = (averageData * (dataAmount - 1) + data.pm2_5) / dataAmount;
+    airQualityGauge.update(averageData);
   };
   client.onConnectionLost = function(response) {
     response.errorCode && connectServer();
